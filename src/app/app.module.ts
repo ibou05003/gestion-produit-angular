2c6b44164bd40c82fa44590dcd7606ffdf77040f
El Hadji Ibrahima NDAO <indao@peopleinput.com>
2021-08-26 18:32:35 +0000

    gestion employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employee.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpErrorResponse, HttpClient } from '@angular/common/http';
+import { throwError, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  public resourceUrl = environment.API_URL+"employee";
+
+  constructor(private http:HttpClient) { }
+
+  /**créer employee */
+  create(employee: any): Observable<any> {
+    return this.http.post<any>(this.resourceUrl, employee);
+  }
+
+  /**modifier employee */
+  update(employee: any, id: any): Observable<any> {
+    return this.http.post<any>(`${this.resourceUrl}/${id}`, employee);
+  }
+
+  /**rechercher employee */
+  find(id: string): Observable<any> {
+    return this.http.get<any>(`${this.resourceUrl}/${id}`);
+  }
+
+  /**Supprimer employee */
+  delete(id: string): Observable<{}> {
+    return this.http.delete(`${this.resourceUrl}/${id}`);
+  }
+
+
+  /**Liste des employees */
+  getEmployees(): Observable<any>{
+    return this.http.get<any>(this.resourceUrl)
+    .pipe(catchError(this.errorHandler))
+  }
+
+  /**permet la gestion des erreurs */
+  errorHandler(error: HttpErrorResponse) {
+    return throwError(error)
+  }
+}
